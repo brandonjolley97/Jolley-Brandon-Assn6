e40f8de58e7d1314ce17c3889bc1b2e2ca4e9a7f
brandonjolley97 <brandonjolley97@gmail.com>
2018-09-24 18:07:53 -0600

    Experimenting with the formating function

--- task2.py
+++ task2.py
@@ -3,8 +3,8 @@
 employeeName = input("Please enter the employee's name: ")
 hoursWorked = eval(input("Please enter the number of hours worked in a week: "))
 payRate = eval(input("Please enter the hourly pay rate: "))
-federalTax = eval(input("Please enter the federal withholding rate as a decimal (ex. 0.12): "))
-stateTax = eval(input("Please enter the state withholding rate as a decimal (ex. 0.12): "))
+federalTax = eval(input("Please enter the federal withholding rate as a decimal (ex. 0.11): "))
+stateTax = eval(input("Please enter the state withholding rate as a decimal (ex. 0.11): "))
 
 # Computing desired output according to user info.
 grossPay = hoursWorked * payRate
@@ -13,13 +13,23 @@ stateDeductions = stateTax * grossPay
 totalDeductions = federalDeductions + stateDeductions
 netPay = grossPay - totalDeductions
 
-# Formatting text to follow the example
-
-
 
 # Displaying results to user.
-print("Employee Name: " + employeeName + "\n" + "Hours Worked: " + str(hoursWorked) + "\n" + "Pay Rate: " + str(payRate) + "\n" + "Gross Pay: " + str(grossPay) + "\n" + "\n"
-      + "Federal Withholding: " + str(federalDeductions) + "\n" + "State Withholding: " + str(stateDeductions) + "\n" "Total Withholding: " + str(totalDeductions) + "\n" + "Net Pay: " + str(netPay))
+
+
+employeeInfo = employeeName.upper() + " PAY INFORMATION" + "\n\n"
+grossPayInfo = "Hours Worked: " + str(hoursWorked) + "\n" + "Pay Rate: " + str(payRate) + "\n" + "Gross Pay: " + str(grossPay) + "\n\n"
+deductionsInfo = "Federal Withholding: " + str(federalDeductions) + "\n" + "State Withholding: " + str(stateDeductions) + "\n" "Total Withholding: " + str(totalDeductions) + "\n"
+netInfo = "Net Pay: " + str(netPay)
+
+# Formatting text to follow the example
+print("\n\n" + format(employeeInfo, "^40"))
+print(format("Pay", "^45"))
+
+print(format("Deductions", "^45"))
+
+
+
 
 
 
